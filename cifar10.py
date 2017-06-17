87a42a696bb3b7bdb3067902feb4ea50782a4581
dongzhuoyao <929109550@qq.com>
2017-06-17 15:08:27 +0800

    update

--- cifar10.py
+++ cifar10.py
@@ -353,7 +353,8 @@ def train(total_loss, global_step):
   # Compute gradients.
   with tf.control_dependencies([loss_averages_op]):
     #opt = tf.train.GradientDescentOptimizer(lr)
-    opt = tf.train.MomentumOptimizer(lr,0.9)
+    #opt = tf.train.MomentumOptimizer(lr,0.9)
+    opt = tf.train.AdamOptimizer(lr)
     grads = opt.compute_gradients(total_loss)
 
   # Apply gradients.
